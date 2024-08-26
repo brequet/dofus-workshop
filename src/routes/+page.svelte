b128118959928e796e4b10642ecfe49f217333e9
brequet <requet.baptiste@gmail.com>
2024-08-26 19:55:25 +0200

    PoC image serving SSR

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let imageId = $state(1001)
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input type="number" bind:value={imageId} />
+<br/>
+<img src="/api/image/{imageId}" alt="Resource image" />
